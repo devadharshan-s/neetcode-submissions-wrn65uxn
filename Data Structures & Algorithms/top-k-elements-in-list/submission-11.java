6e74a3c2ad7b95f1d3cf49010f4834820cba5968
devadharshan-s <104019511+devadharshan-s@users.noreply.github.com>
2026-04-05 16:18:09 +0530

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        // TC : O(n)[map building] + O(n) [pushing into heap]
+        // SC : O(n)[map] + O(k) heap 
+        int[] res = new int[k];
+        int n = nums.length;
+
+        Map<Integer,Integer> m = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);
+        }
+
+        ArrayList<Integer>[] bucket = new ArrayList[n + 1];
+
+        for(int key : m.keySet()){
+            int val = m.get(key);
+            if(bucket[val] == null) bucket[val] = new ArrayList<>();
+            bucket[val].add(key);
+        }
+
+        int idx = 0;
+        for(int i = n; i >= 0; i--){
+            if(bucket[i] == null) continue;
+            for(Integer j : bucket[i]){                
+                if(idx == k) break;
+                res[idx++] = j;
+            }
+            if(idx == k) break;
+        } 
+        // [1,1]
+        // [2,2]
+        // [3,3]
+
+        // [1,1]
+        // [2,2]
+        // [3,3]
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+
+        // for(int key : m.keySet()){
+        //     pq.add(new int[]{m.get(key),key});
+        //     if(pq.size() > k) pq.poll();
+        // }
+
+        // // while(pq.size() > k) pq.poll();
+        // int idx = 0;
+
+        // while(k-- > 0){
+        //     res[idx++] = pq.poll()[1];
+        // }
+
+        return res;
+    }
+}
